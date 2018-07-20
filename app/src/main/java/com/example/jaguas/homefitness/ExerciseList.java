567fddced265d5e6d7ea7c2cacdc92ee62d3f04d
JuanAguas <35305790+cdajp4ever@users.noreply.github.com>
2018-07-20 09:06:03 -0700

    Fixed onClick on alertDialog, pending to create Intent extra to pass the information for the animation on the MainExercise screen

--- app/src/main/java/com/example/jaguas/homefitness/ExerciseList.java
+++ app/src/main/java/com/example/jaguas/homefitness/ExerciseList.java
@@ -1,6 +1,9 @@
 package com.example.jaguas.homefitness;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -12,19 +15,13 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class ExerciseList extends AppCompatActivity {
 
-    // TEST COMMIT AND PUSH
-
     private String[] kinds = {"Belly", "Back", "Chest", "Arms", "Legs"};
-    private String[] belly = {"Crunchers", "Reverse Crunchers", "Flutter Kicks", "Sitting Twists", "Knee to Elbow"};
-    private String[] back = {"Reverse Snow Angels", "Superman", "Hip Hinge", "Flying Passe", "Twister"};
-    private String[] chest = {"Rotational press-up", "Single Leg Push Up", "Wide Push Up", "Narrow/Tricep Push Up", "Diamond push-up"};
-    private String[] arms = {"Push-Ups", "One-Arm Side Push-Up", "The Windmill", "The Wave Goodbye", "Close-grip pushup"};
-    private String[] legs = {"Single-leg circle", "Squat Jump", "Side Lunge", "Single-Leg Hip Raise", "Alternating Drop Lunge"};
     private TextView pos1, pos2, pos3, pos4, pos5;
 
     private int[] img = {R.drawable.belly, R.drawable.back, R.drawable.chest, R.drawable.arms,
@@ -42,85 +39,73 @@ public class ExerciseList extends AppCompatActivity {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                pos1 = findViewById(R.id.one);
-                pos2 = findViewById(R.id.two);
-                pos3 = findViewById(R.id.three);
-                pos4 = findViewById(R.id.four);
-                pos5 = findViewById(R.id.five);
-                AlertDialog.Builder builder = new AlertDialog.Builder(ExerciseList.this);
-                LayoutInflater inflater = getLayoutInflater();
-                View dialogView = inflater.inflate(R.layout.dialog, null);
 
-                builder.setView(dialogView);
                 if(position == 0) {
-                    final TextView textView1 = dialogView.findViewById(R.id.one);
-                    textView1.setText(belly[0]);
-                    final TextView textView2 = dialogView.findViewById(R.id.two);
-                    textView2.setText(belly[1]);
-                    final TextView textView3 = dialogView.findViewById(R.id.three);
-                    textView3.setText(belly[2]);
-                    final TextView textView4 = dialogView.findViewById(R.id.four);
-                    textView4.setText(belly[3]);
-                    final TextView textView5 = dialogView.findViewById(R.id.five);
-                    textView5.setText(belly[4]);
-                    final AlertDialog alertDialog = builder.create();
-                    alertDialog.show();
+                    AlertDialog.Builder builder = new AlertDialog.Builder(ExerciseList.this);
+                    builder.setTitle(R.string.selectExecise)
+                            .setItems(R.array.belly, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int which) {
+                                    switch (which){
+                                        case 0:
+
+                                            //need to check where to put the initial intent and
+                                            // then put the extra to change the recycler view
+                                    }
+                                }
+                            });
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
 
                 } else if (position == 1) {
-                    final TextView textView1 = dialogView.findViewById(R.id.one);
-                    textView1.setText(back[0]);
-                    final TextView textView2 = dialogView.findViewById(R.id.two);
-                    textView2.setText(back[1]);
-                    final TextView textView3 = dialogView.findViewById(R.id.three);
-                    textView3.setText(back[2]);
-                    final TextView textView4 = dialogView.findViewById(R.id.four);
-                    textView4.setText(back[3]);
-                    final TextView textView5 = dialogView.findViewById(R.id.five);
-                    textView5.setText(back[4]);
-                    final AlertDialog alertDialog = builder.create();
-                    alertDialog.show();
+                    AlertDialog.Builder builder = new AlertDialog.Builder(ExerciseList.this);
+                    builder.setTitle(R.string.selectExecise)
+                            .setItems(R.array.back, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
 
+                                }
+                            });
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
                 } else if (position == 2) {
-                    final TextView textView1 = dialogView.findViewById(R.id.one);
-                    textView1.setText(chest[0]);
-                    final TextView textView2 = dialogView.findViewById(R.id.two);
-                    textView2.setText(chest[1]);
-                    final TextView textView3 = dialogView.findViewById(R.id.three);
-                    textView3.setText(chest[2]);
-                    final TextView textView4 = dialogView.findViewById(R.id.four);
-                    textView4.setText(chest[3]);
-                    final TextView textView5 = dialogView.findViewById(R.id.five);
-                    textView5.setText(chest[4]);
-                    final AlertDialog alertDialog = builder.create();
-                    alertDialog.show();
+                    AlertDialog.Builder builder = new AlertDialog.Builder(ExerciseList.this);
+                    builder.setTitle(R.string.selectExecise)
+                            .setItems(R.array.chest, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+
+                                }
+                            });
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+
 
                 } else if (position == 3) {
-                    final TextView textView1 = dialogView.findViewById(R.id.one);
-                    textView1.setText(arms[0]);
-                    final TextView textView2 = dialogView.findViewById(R.id.two);
-                    textView2.setText(arms[1]);
-                    final TextView textView3 = dialogView.findViewById(R.id.three);
-                    textView3.setText(arms[2]);
-                    final TextView textView4 = dialogView.findViewById(R.id.four);
-                    textView4.setText(arms[3]);
-                    final TextView textView5 = dialogView.findViewById(R.id.five);
-                    textView5.setText(arms[4]);
-                    final AlertDialog alertDialog = builder.create();
-                    alertDialog.show();
+                    AlertDialog.Builder builder = new AlertDialog.Builder(ExerciseList.this);
+                    builder.setTitle(R.string.selectExecise)
+                            .setItems(R.array.arms, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+
+                                }
+                            });
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+
 
                 } else if (position == 4) {
-                    final TextView textView1 = dialogView.findViewById(R.id.one);
-                    textView1.setText(legs[0]);
-                    final TextView textView2 = dialogView.findViewById(R.id.two);
-                    textView2.setText(legs[1]);
-                    final TextView textView3 = dialogView.findViewById(R.id.three);
-                    textView3.setText(legs[2]);
-                    final TextView textView4 = dialogView.findViewById(R.id.four);
-                    textView4.setText(legs[3]);
-                    final TextView textView5 = dialogView.findViewById(R.id.five);
-                    textView5.setText(legs[4]);
-                    final AlertDialog alertDialog = builder.create();
-                    alertDialog.show();
+                    AlertDialog.Builder builder = new AlertDialog.Builder(ExerciseList.this);
+                    builder.setTitle(R.string.selectExecise)
+                            .setItems(R.array.legs, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+
+                                }
+                            });
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+
 
                 }
             }
@@ -148,11 +133,5 @@ public class ExerciseList extends AppCompatActivity {
             return convertView;
         }
     }
-    public void startExercising(View view){
-        Intent intent = new Intent(this,MainExercise.class);
 
-        switch (view.getId()){
-            case R.id.one:
-        }
-    }
 }
